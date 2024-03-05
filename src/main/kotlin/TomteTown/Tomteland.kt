34c15485b8d25063cd18a58fa38b3e82d4926b5d
Simon Nordlund <simon_nordlund@hotmail.com>
2024-03-05 15:42:47 +0100

    Redigerat del av kod7

--- src/main/kotlin/TomteTown/Tomteland.kt
+++ src/main/kotlin/TomteTown/Tomteland.kt
@@ -3,32 +3,14 @@ package TomteTown
 class Tomteland {
 
     val mapTomteTown = mapOf(
-        "Tomten" to listOf("Glader", "Butter"), "Glader" to listOf("Tröger", "Trötter", "Blyger"),
-        "Butter" to listOf("Rådjuret", "Nyckelpigan", "Haren", "Räven"), "Trötter" to listOf("Skumtomten"),
-        "Skumtomten" to listOf("Dammråttan"), "Räven" to listOf("Gråsuggan", "Myran"), "Myran" to listOf("Bladlusen")
+        "Tomten" to listOf("Glader", "Butter"),
+        "Glader" to listOf("Tröger", "Trötter", "Blyger"),
+        "Butter" to listOf("Rådjuret", "Nyckelpigan", "Haren", "Räven"),
+        "Trötter" to listOf("Skumtomten"),
+        "Skumtomten" to listOf("Dammråttan"),
+        "Räven" to listOf("Gråsuggan", "Myran"),
+        "Myran" to listOf("Bladlusen")
     )
-
-    fun getUnderlings(tomteName: String, mapTomteTown: Map<String, List<String>>): List<String> {
-
-        tailrec fun findUnderlings(tomteSearch: List<String>, underlingsFound: List<String>): List<String> {
-            return if (tomteSearch.isEmpty()) underlingsFound ///När tomteSearch är tom, return underlings/ackumulator.
-
-            else {
-                //Tar fram tomtenyckeln som ska sökas på.
-                val tomteKey = tomteSearch.first()
-                //Letar upp listan som tillhör nyckeln. Om ej finns någon och det blir null -> TomLista.
-                val newUnderlings = mapTomteTown[tomteKey] ?: listOf()
-                //Tar bort söktTomte och lägger till nästa ev. Tomte/Tomtar. Tom lista + befintlig lista = oförändrat.
-                val newTomteSearch = tomteSearch.drop(1) + newUnderlings
-                //Slår ihop nya underlings med de nuvarande.
-                val newUnderlingsFound = underlingsFound + newUnderlings
-
-                //printValuesFromFunction(tomteKey, newUnderlings, newTomteSearch, newUnderlingsFound)
-                return findUnderlings(newTomteSearch, newUnderlingsFound)
-            }
-        }
-        return findUnderlings(listOf(tomteName), listOf())
-    }
 
     fun askForTomte(isTest: Boolean): String {
         return if (isTest) {
@@ -46,5 +28,27 @@ class Tomteland {
         println("Detta är newUnderlings: $newUL")
         println("Detta är newTomteSearch: $newTS")
         println("Detta är newUnderlingsFound: $newULF")
+    }
+
+    fun getUnderlings(tomteName: String, mapTomteTown: Map<String, List<String>>): List<String> {
+
+        tailrec fun findUnderlings(tomteSearch: List<String>, underlingsFound: List<String>): List<String> {
+            return if (tomteSearch.isEmpty()) underlingsFound ///När tomteSearch är tom, return underlings/ackumulator.
+
+            else {
+                //Tar fram tomtenyckeln som ska sökas på.
+                val tomteKey = tomteSearch.first()
+                //Letar upp listan som tillhör nyckeln. Om ej finns någon och det blir null -> TomLista.
+                val newUnderlings = mapTomteTown[tomteKey] ?: listOf()
+                //Tar bort söktTomte och lägger till nästa ev. Tomte/Tomtar. Tom lista + befintlig lista = oförändrat.
+                val newTomteSearch = tomteSearch.drop(1) + newUnderlings
+                //Slår ihop nya underlings med de nuvarande.
+                val newUnderlingsFound = underlingsFound + newUnderlings
+
+                printValuesFromFunction(tomteKey, newUnderlings, newTomteSearch, newUnderlingsFound)
+                return findUnderlings(newTomteSearch, newUnderlingsFound)
+            }
+        }
+        return findUnderlings(listOf(tomteName), listOf())
     }
 }
